266d5273ed32068d5a636cf6ba560b54d510be83
sechshelme <m_burkhard@gmx.ch>
2020-08-28 16:00:44 +0200

    test

--- 99_Test/10_Menu_aendern/Project1.pas
+++ 99_Test/10_Menu_aendern/Project1.pas
@@ -24,7 +24,8 @@ type
     procedure InitMenuBar; virtual;                    // Menü
     procedure HandleEvent(var Event: TEvent); virtual; // Eventhandler
 
-    procedure MyParameter;                             // neue Funktion für einen Dialog.
+    procedure MyParameter;
+    // neue Funktion für einen Dialog.
   end;
 
   procedure TMyApp.InitStatusLine;
@@ -45,36 +46,36 @@ type
     StatusLine := New(PStatusLine, Init(Rect, P0));
   end;
 
-    procedure TMyApp.InitMenuBar;
-    var
-      Rect: TRect;                       // Rechteck für die Menüzeilen-Position.
+  procedure TMyApp.InitMenuBar;
+  var
+    Rect: TRect;                       // Rechteck für die Menüzeilen-Position.
 
-      M: PMenu;                          // Ganzes Menü
-      SM0, SM1,                          // Submenu
-      M0_0, M0_1, M0_2, M0_3,M0_4, M0_5,
-      M1_0: PMenuItem;                   // Einfache Menüpunkte
+    M: PMenu;                          // Ganzes Menü
+    SM0, SM1,                          // Submenu
+    M0_0, M0_1, M0_2, M0_3, M0_4, M0_5, M1_0: PMenuItem;
+    // Einfache Menüpunkte
 
-    begin
-      GetExtent(Rect);
-      Rect.B.Y := Rect.A.Y + 1;
+  begin
+    GetExtent(Rect);
+    Rect.B.Y := Rect.A.Y + 1;
 
-      M1_0 := NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil);
-      SM1 := NewSubMenu('~H~ilfe', hcNoContext, NewMenu(M1_0), nil);
+    M := NewMenu(NewSubMenu('~D~atei', hcNoContext,
+    NewMenu(NewItem('~M~enu ändern', 'F2', kbF2, cmList, hcNoContext,
+      NewItem('~P~arameter...', '', kbF2, cmPara, hcNoContext,
+      NewLine(
+      NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext,
+      NewLine(
+      NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil))))))),
+    NewSubMenu('~H~ilfe', hcNoContext,
+    NewMenu(NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil)));
 
-      M0_5 := NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil);
-      M0_4 := NewLine(M0_5);
-      M0_3 := NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext, M0_4);
-      M0_2 := NewLine(M0_3);
-      M0_1:=NewItem('~P~arameter...', '', kbF2, cmPara, hcNoContext, M0_2);
-      M0_0 := NewItem('~L~iste', 'F2', kbF2, cmList, hcNoContext, M0_1);
-      SM0 := NewSubMenu('~D~atei', hcNoContext, NewMenu(M0_0), SM1);
-
-      M := NewMenu(SM0);
-
-      MenuBar := New(PMenuBar, Init(Rect, M));
-    end;
+    MenuBar := New(PMenuBar, Init(Rect, M));
+  end;
 
   procedure TMyApp.HandleEvent(var Event: TEvent);
+  var
+    r: TRect;
+    m: PMenuItem;
   begin
     inherited HandleEvent(Event);
 
@@ -83,6 +84,14 @@ type
         cmAbout: begin
         end;
         cmList: begin
+
+          GetExtent(r);
+          r.B.Y := r.A.Y + 1;
+          m := NewItem('Close', '', kbAltX, cmPara, hcNoContext, nil);
+          MenuBar := New(PMenuBar, Init(r, NewMenu(m)));
+//          Dispose(MenuBar, Done);
+          insert(MenuBar);
+//                    ReDraw;
         end;
         cmPara: begin
           MyParameter;
