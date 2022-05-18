da7afef19b830372e42b81d02a69ceff9e63111a
sechshelme <m_burkhard@gmx.ch>
2022-05-18 15:21:40 +0200

    ListBox

---
+++ 99_Test/10_ListBox/mydialog.pas
@@ -0,0 +1,108 @@
+//head+
+unit MyDialog;
+//head-
+
+interface
+
+uses
+  App, Objects, Drivers, Views, MsgBox, Dialogs,
+  SysUtils; // Für IntToStr und StrToInt.
+
+//type+
+type
+  PMyDialog = ^TMyDialog;
+
+  TMyDialog = object(TDialog)
+  const
+    cmCounter = 1003;       // Wird lokal für den Zähler-Butoon gebraucht.
+  var
+    CounterButton: PButton; // Button mit Zähler.
+
+    constructor Init;
+    procedure HandleEvent(var Event: TEvent); virtual;
+  end;
+//type-
+
+implementation
+
+//init+
+constructor TMyDialog.Init;
+var
+  Rect: TRect;
+  sb: PScrollBar;
+  ListBox: PListBox;
+  pc: PCollection;
+  ps: PString;
+begin
+  Rect.Assign(0, 0, 42, 21);
+  Rect.Move(23, 3);
+  inherited Init(Rect, 'Mein Dialog');
+
+  // ListBox
+  Rect.Assign(5, 5, 7, 15);
+  sb := new(PScrollBar, Init(Rect));
+
+  pc := new(PCollection, Init(4, 4));
+  pc^.Insert(NewStr('abc'));
+  pc^.Insert(NewStr('abc'));
+  pc^.Insert(NewStr('abc'));
+  pc^.Insert(NewStr('abc'));
+
+  Rect.Assign(5, 2, 31, 15);
+  ListBox := new(PListBox, Init(Rect, 3, nil));
+  //  ListBox^.NewList(pc);
+  //  ps:=new(PString);
+  //  ps^:='abcd';
+
+  //  ListBox^.Insert(ps);
+  //  ListBox^.Insert(ps);
+  ps := newstr('dsfdsfdsf');
+  //  ListBox^.List^.Insert(ps);
+  ListBox^.Insert(pc);
+  //ListBox^.Insert(PString, Init('hallo'));
+  //ListBox^.Insert(PString, Init('hallo'));
+
+  Insert(ListBox);
+
+  // Button, bei den der Titel geändert wird.
+  Rect.Assign(19, 18, 32, 20);
+  CounterButton := new(PButton, Init(Rect, '    ', cmCounter, bfNormal));
+  CounterButton^.Title^ := '1';
+
+  Insert(CounterButton);
+
+  // Ok-Button
+  Rect.Assign(7, 18, 17, 20);
+  Insert(new(PButton, Init(Rect, '~O~K', cmOK, bfDefault)));
+end;
+//init-
+
+//handleevent+
+procedure TMyDialog.HandleEvent(var Event: TEvent);
+var
+  Counter: integer;
+begin
+  inherited HandleEvent(Event);
+
+  case Event.What of
+    evCommand: begin
+      case Event.Command of
+        cmCounter: begin
+          Counter := StrToInt(CounterButton^.Title^); // Titel des Button auslesen.
+          Inc(Counter);                               // Counter erhöhen.
+          if Counter > 9999 then begin                // Auf Überlauf prüfen, weil nur 4 Zeichen zur Verfügung.
+            Counter := 9999;
+          end;
+          CounterButton^.Title^ := IntToStr(Counter); // Neuer Titel an Button übergeben.
+
+          CounterButton^.Draw;                        // Button neu zeichnen.
+          ClearEvent(Event);                          // Event beenden.
+        end;
+      end;
+    end;
+  end;
+
+end;
+//handleevent-
+
+end.
