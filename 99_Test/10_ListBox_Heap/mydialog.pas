7a110c824718eaac51801756b1458daeacaf2748
sechshelme <m_burkhard@gmx.ch>
2022-05-20 17:07:02 +0200

    ListBox

--- 99_Test/10_ListBox_Heap/mydialog.pas
+++ 99_Test/10_ListBox_Heap/mydialog.pas
@@ -12,16 +12,6 @@ uses
   App, Objects, Drivers, Views, Dialogs, MsgBox, StdDlg,
   SysUtils; // Für IntToStr und StrToInt.
 
-type
-
-  { TNewListBox }
-
-  PNewListBox = ^TNewListBox;
-  TNewListBox = object(TSortedListBox)
-    destructor Done; virtual;
-  end;
-
-
 //type+
 type
   PMyDialog = ^TMyDialog;
@@ -30,7 +20,7 @@ type
   const
     cmTag = 1000;
   var
-    ListBox: PNewListBox;
+    ListBox: PListBox;
 
     constructor Init;
     procedure HandleEvent(var Event: TEvent); virtual;
@@ -39,35 +29,29 @@ type
 
 implementation
 
-{ TNewListBox }
-
-destructor TNewListBox.Done;
-begin
-//  if List <> nil then Dispose(List, Done);
-//  TListBox.Done;
-//  FreeAll;
-  inherited Done;
-end;
-
 //init+
 constructor TMyDialog.Init;
 var
   Rect: TRect;
   ScrollBar: PScrollBar;
   StringCollection: PCollection;
+  I: Sw_Integer;
 
 begin
   Rect.Assign(10, 5, 67, 17);
   inherited Init(Rect, 'ListBox Demo');
 
-  //  Title := NewStr('dfsfdsa');
+  if Title <> nil then begin
+    Dispose(Title);
+  end;
+  Title := NewStr('dfsfdsa');
 
   // ListBox
   Rect.Assign(31, 2, 32, 7);
   ScrollBar := new(PScrollBar, Init(Rect));
   Insert(ScrollBar);
 
-//  StringCollection := new(PCollection, Init(2, 1));
+  //  StringCollection := new(PCollection, Init(2, 1));
   StringCollection := new(PCollection, Init(4, 1));
   StringCollection^.Insert(NewStr('Montag'));
   StringCollection^.Insert(NewStr('Dienstag'));
@@ -78,10 +62,35 @@ begin
   StringCollection^.Insert(NewStr('Sonntag'));
 
   Rect.Assign(5, 2, 31, 7);
-  ListBox := new(PNewListBox, Init(Rect, 1, ScrollBar));
+  ListBox := new(PListBox, Init(Rect, 1, ScrollBar));
   ListBox^.NewList(StringCollection);
 
   Insert(ListBox);
+
+  //  ListBox^.FreeAll;
+
+  //if (ListBox^.List <> nil) then begin
+  //  for I := ListBox^.List^.Count - 1 downto 0 do begin
+  //    MessageBox(i.ToString, nil, mfOKButton);
+  //    MessageBox(Pstring(ListBox^.List^.At(I))^, nil, mfOKButton);
+  ////    ListBox^.List^.FreeItem(ListBox^.List^.At(I));
+  //  end;
+  //  ListBox^.List^.Count := 0;                                        { Clear item count }
+  //
+  //
+  //
+  //  MessageBox('Wochentag', nil, mfOKButton);
+  //  ListBox^.List^.FreeAll;
+  //  MessageBox('Wochentag', nil, mfOKButton);
+  //  ListBox^.SetRange(ListBox^.List^.Count);
+  //  MessageBox('Wochentag', nil, mfOKButton);
+  //end;
+  //
+  //
+  //
+  //MessageBox('Wochentag', nil, mfOKButton);
+
+
   //ListBox^.Insert(NewStr('aaaaaaaaa'));
   //
   //ListBox^.List^.Insert(NewStr('bbbbbbb'));
@@ -105,13 +114,13 @@ var
   s: string;
 
 begin
-  inherited HandleEvent(Event);
 
   case Event.What of
     evCommand: begin
       case Event.Command of
         cmOK: begin
-//          MessageBox('Wochentag', nil, mfOKButton);
+//          ListBox^.FreeAll;
+          //          MessageBox('Wochentag', nil, mfOKButton);
         end;
 
         cmTag: begin
@@ -122,6 +131,7 @@ begin
       end;
     end;
   end;
+  inherited HandleEvent(Event);
 
 end;
 //handleevent-
