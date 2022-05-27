593dca1fd4ad9ddbf7e8f7d5972e586987287900
sechshelme <m_burkhard@gmx.ch>
2022-05-27 17:05:37 +0200

    tlistbox

---
+++ 20_Diverses/00_Idle_Handle_eine_Uhr/uhrdialog.pas
@@ -0,0 +1,82 @@
+//head+
+unit UhrDialog;
+//head-
+
+interface
+
+uses
+  SysUtils,
+  Drivers, Objects, Views, Dialogs;
+
+//type+
+const
+  cmUhrRefresh = 1003;
+
+type
+  PUhrView = ^TUhrView;
+  TUhrView = object(TDialog)
+  private
+    ZeitStr: ShortString;
+  public
+    constructor Init;
+    procedure Draw; Virtual;
+    procedure HandleEvent(var Event: TEvent); virtual;
+  end;
+//type-
+
+implementation
+
+//init+
+constructor TUhrView.Init;
+var
+  R: TRect;
+begin
+  R.Assign(51, 1, 70, 8);
+  inherited Init(R, 'Uhr');
+
+  R.Assign(7, 4, 13, 6);
+  Insert(new(PButton, Init(R, '~O~k', cmOK, bfDefault)));
+end;
+//init-
+
+//draw+
+procedure TUhrView.Draw;
+var
+  b: TDrawBuffer;
+  c: Byte;
+begin
+  inherited Draw;
+  c := GetColor(7);
+  MoveChar(b, ' ', c, Size.X + 4);
+  MoveStr(b, ZeitStr, c);
+  WriteLine(5, 2, Size.X + 2, 1, b);
+end;
+//draw-
+
+//handleevent+
+procedure TUhrView.HandleEvent(var Event: TEvent);
+begin
+  inherited HandleEvent(Event);
+
+  case Event.What of
+    evBroadcast: begin
+      case Event.Command of
+        cmUhrRefresh: begin
+          ZeitStr := PString(Event.InfoPtr)^;
+          Draw;
+        end;
+      end;
+    end;
+    evCommand: begin
+      case Event.Command of
+        cmOK: begin
+          Close;
+        end;
+      end;
+    end;
+  end;
+end;
+//handleevent-
+
+end.
+
---
+++ 30_Gadgets/00_RAM_Verbrauch_anzeigen_(Heap)/mydialog.pas
@@ -0,0 +1,109 @@
+//head+
+unit MyDialog;
+//head-
+
+interface
+
+{$H-}
+
+uses
+  App, Objects, Drivers, Views, Dialogs, MsgBox, StdDlg;
+
+//type+
+type
+  PMyDialog = ^TMyDialog;
+
+  { TMyDialog }
+
+  TMyDialog = object(TDialog)
+  const
+    cmTag = 1000;  // Lokale Event Konstante
+  var
+    ListBox: PListBox;
+    StringCollection: PStringCollection;
+
+    constructor Init;
+    destructor Done; virtual;  // Wegen Speicher Leak
+    procedure HandleEvent(var Event: TEvent); virtual;
+  end;
+//type-
+
+implementation
+
+//init+
+constructor TMyDialog.Init;
+var
+  R: TRect;
+  ScrollBar: PScrollBar;
+  i: Sw_Integer;
+const
+  Tage: array [0..6] of shortstring = (
+    'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag');
+
+begin
+  R.Assign(10, 5, 64, 17);
+  inherited Init(R, 'ListBox Demo');
+
+  // StringCollection
+  StringCollection := new(PStringCollection, Init(5, 5));
+  for i := 0 to Length(Tage) - 1 do begin
+    StringCollection^.Insert(NewStr(Tage[i]));
+  end;
+
+  // ScrollBar für ListBox
+  R.Assign(31, 2, 32, 7);
+  ScrollBar := new(PScrollBar, Init(R));
+  Insert(ScrollBar);
+
+  // ListBox
+  R.Assign(5, 2, 31, 7);
+  ListBox := new(PListBox, Init(R, 1, ScrollBar));
+  ListBox^.NewList(StringCollection);
+  Insert(ListBox);
+
+  // Tag-Button
+  R.Assign(5, 9, 18, 10);
+  Insert(new(PButton, Init(R, '~T~ag', cmTag, bfNormal)));
+
+  // Cancel-Button
+  R.Move(15, 0);
+  Insert(new(PButton, Init(R, '~C~ancel', cmCancel, bfNormal)));
+
+  // Ok-Button
+  R.Move(15, 0);
+  Insert(new(PButton, Init(R, '~O~K', cmOK, bfDefault)));
+end;
+//init-
+
+//done+
+destructor TMyDialog.Done;
+begin
+   Dispose(ListBox^.List, Done); // Dies Versuchsweise ausklammern
+   inherited Done;
+end;
+//done-
+
+//handleevent+
+procedure TMyDialog.HandleEvent(var Event: TEvent);
+var
+  s: ShortString;
+begin
+  case Event.What of
+    evCommand: begin
+      case Event.Command of
+        cmOK: begin
+          // mache etwas
+        end;
+        cmTag: begin
+          str(ListBox^.Focused + 1, s);
+          MessageBox('Wochentag: ' + s + ' gew' + #132 + 'hlt', nil, mfOKButton);
+          ClearEvent(Event);  // Event beenden.
+        end;
+      end;
+    end;
+  end;
+  inherited HandleEvent(Event);
+end;
+//handleevent-
+
+end.
