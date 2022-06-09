9c4166cd9a9906d232ef4b9f5f7ce3a0e7b56c8c
sechshelme <m_burkhard@gmx.ch>
2022-06-09 15:59:02 +0200

    test

--- Tools/Create_github_wiki_class_tree/unit1.pas
+++ Tools/Create_github_wiki_class_tree/unit1.pas
@@ -14,7 +14,7 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Button2: TButton;
-    CheckBox1: TCheckBox;
+    Borland: TCheckBox;
     Memo1: TMemo;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
@@ -37,47 +37,61 @@ procedure TForm1.Button1Click(Sender: TObject);
 var
   fsl, sl: TStringList;
   i, j, p: integer;
-  s, st: string;
+  uname, s, st: string;
   sa: TStringArray;
 begin
   Memo1.Clear;
   Memo1.Lines.Add('```mermaid');
   Memo1.Lines.Add('classDiagram');
   Memo1.Lines.Add('direction RL');
-  //  fsl := FindAllFiles('/home/tux/fpcupdeluxe_avr5/fpcsrc/packages/fv/src', '*.pas;*.inc');
-  fsl := FindAllFiles('/home/tux/fpcupdeluxe_avr5/fpcsrc/packages/fv/src', 'views.inc');
+  if Borland.Checked then begin
+    fsl := FindAllFiles('/n4800/dos/bp70/DISK12/RTLTV', '*.PAS');
+  end else begin
+    fsl := FindAllFiles('/home/tux/fpcupdeluxe_avr5/fpcsrc/packages/fv/src', '*.pas;*.inc');
+  end;
   for i := 0 to fsl.Count - 1 do begin
+    uname := '';
     sl := TStringList.Create;
     sl.LoadFromFile(fsl[i]);
     for j := 0 to sl.Count - 1 do begin
       s := sl[j];
+        sa := s.Split([' ', ';']);
+        if Length(sa) >= 2 then begin
+          if UpCase(sa[0])='UNIT' then    begin
+          uname := sa[1];
+          Memo1.Lines.Add('...............:' + uname);
+          end;
+        end;
       s := StringReplace(s, ' ', '', [rfIgnoreCase, rfReplaceAll]);
       if Pos('OBJECT(T', UpCase(s)) > 0 then begin
         sa := s.Split(['=', '(', ')']);
         if Length(sa) >= 3 then begin
           Memo1.Lines.Add(sa[0] + '<..' + sa[2]);
+          if uname <> ' ' then  begin
+            Memo1.Lines.Add(sa[0] + ':' + uname);
+          end;
           st := sa[0];
         end;
-        if CheckBox1.Checked then begin
-          p := j;
-          repeat
-            Inc(p);
-            s := sl[p];
-            s := StringReplace(s, ' ', '', [rfIgnoreCase, rfReplaceAll]);
-            sa := s.Split([':', ';']);
-            if Length(sa) >= 2 then begin
-              if (Pos('CONSTRUCTOR', UpCase(sl[p])) > 0) or (Pos('DESTRUCTOR', UpCase(sl[p])) > 0) or (Pos('FUNCTION', UpCase(sl[p])) > 0) or
-                (Pos('PROCEDURE', UpCase(sl[p])) > 0) then begin
-//                Memo1.Lines.Add(st + ':' + sa[1] + '()');
-              end else begin
-                Memo1.Lines.Add(st + ':' + sa[0] + ' ' + sa[1]);
-              end;
-            end;
-
-
-          until Pos(' END;', UpCase(sl[p])) > 0;
-
-        end;
+        //if Borland.Checked then begin
+        //  p := j;
+        //  repeat
+        //    Inc(p);
+        //    s := sl[p];
+        //    s := StringReplace(s, ' ', '', [rfIgnoreCase, rfReplaceAll]);
+        //    sa := s.Split([':', ';']);
+        //    if Length(sa) >= 2 then begin
+        //      if (Pos('CONSTRUCTOR', UpCase(sl[p])) > 0) or (Pos('DESTRUCTOR', UpCase(sl[p])) > 0) or (Pos('FUNCTION', UpCase(sl[p])) > 0) or
+        //        (Pos('PROCEDURE', UpCase(sl[p])) > 0) then begin
+        //        //                Memo1.Lines.Add(st + ':' + sa[1] + '()');
+        //      end else begin
+        //        Memo1.Lines.Add(st + ':' + sa[0] + ' ' + sa[1]);
+        //      end;
+        //    end;
+        //
+        //
+        //  until Pos(' END;', UpCase(sl[p])) > 0;
+        //
+        //end;
 
       end;
     end;
@@ -89,8 +103,8 @@ end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 begin
-  if CheckBox1.Checked then begin
-    Memo1.Lines.SaveToFile('/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/FreeVision/Class_Tree_with_identifier.md');
+  if Borland.Checked then begin
+    Memo1.Lines.SaveToFile('/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/FreeVision/Turbo_Vision_Class_Tree.md');
   end else begin
     Memo1.Lines.SaveToFile('/n4800/DATEN/Programmierung/mit_GIT/Lazarus/Tutorial/FreeVision/Class_Tree.md');
   end;
